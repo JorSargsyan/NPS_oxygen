e8d39c78bc22a6aac27a43358617f3e65dc03fbd
Jora Sargsyan <jorsargsyan@gmail.com>
2023-04-10 16:38:34 +0400

    fix: added new url

--- src/pages/dashboard/CampaignDetails/questions/MainContent/components/MetricForm.tsx
+++ src/pages/dashboard/CampaignDetails/questions/MainContent/components/MetricForm.tsx
@@ -1,9 +1,11 @@
-import { Grid, Typography } from "@mui/material";
+import { Grid, SvgIcon, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useSelector } from "react-redux";
 import { requiredRules } from "shared/helpers/validators";
 import TextInput from "shared/ui/TextInput";
 import { selectSurveyInfo } from "store/slicers/campaignDetail";
+import { ECampaignSurveyType } from "../../LeftSidebar/constants";
+import StarIcon from "@heroicons/react/24/solid/StarIcon";
 
 const MetricForm = () => {
   const surveyInfo = useSelector(selectSurveyInfo);
@@ -14,21 +16,31 @@ const MetricForm = () => {
         <Grid item xs={12}>
           <Box display="flex" justifyContent={"center"} gap={2}>
             {surveyInfo.details.answers.map((answer) => (
-              <Box
-                key={answer.id}
-                p={1}
-                borderRadius={"10px"}
-                minWidth={"40px"}
-                sx={{ backgroundColor: "primary.main" }}
-              >
-                <Typography
-                  fontSize={16}
-                  fontWeight={"bold"}
-                  color="white"
-                  textAlign={"center"}
-                >
-                  {answer.value}
-                </Typography>
+              <Box key={answer.id}>
+                {surveyInfo.details.type ===
+                Number(ECampaignSurveyType.CustomStar) ? (
+                  <Box p={1}>
+                    <SvgIcon color="primary">
+                      <StarIcon height={24} />
+                    </SvgIcon>
+                  </Box>
+                ) : (
+                  <Box
+                    p={1}
+                    borderRadius={"10px"}
+                    minWidth={"40px"}
+                    sx={{ backgroundColor: "primary.main" }}
+                  >
+                    <Typography
+                      fontSize={16}
+                      fontWeight={"bold"}
+                      color="white"
+                      textAlign={"center"}
+                    >
+                      {answer.value}
+                    </Typography>
+                  </Box>
+                )}
               </Box>
             ))}
           </Box>
